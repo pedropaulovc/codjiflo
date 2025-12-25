e364a9acb93da80c0cce590c3e1b786b0f2a0695
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2025-12-24 22:35:55 -0800

    fix(lint): fix eslint errors in tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,18 +1,19 @@
 import "@testing-library/jest-dom/vitest";
-import { vi } from 'vitest';
+
 
 // Mock localStorage
 const localStorageMock = (function () {
     let store: Record<string, string> = {};
     return {
-        getItem: (key: string) => store[key] || null,
+        getItem: (key: string) => store[key] ?? null,
         setItem: (key: string, value: string) => {
-            store[key] = value.toString();
+            store[key] = value;
         },
         clear: () => {
             store = {};
         },
         removeItem: (key: string) => {
+            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
             delete store[key];
         },
     };
